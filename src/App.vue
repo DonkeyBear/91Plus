41494f569920d18529af87f7034a9f251fbc547b
DonkeyBear <35475647+DonkeyBear@users.noreply.github.com>
2025-10-16 10:04:57 +0800

    feat: 整合 VueUse 並優化點擊偵測
    - 新增 @vueuse/core 依賴套件
    - 使用 VueUse 的 onClickOutside 取代自訂的 TriggerOverlay 元件
    - 移除不再需要的 TriggerOverlay 元件檔案

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <script setup>
+import { onClickOutside, useParentElement } from '@vueuse/core'
 import AppFooter from './components/AppFooter.vue'
 import AppHeader from './components/AppHeader.vue'
 import DarkModeOverlay from './components/DarkModeOverlay.vue'
-import TriggerOverlay from './components/TriggerOverlay.vue'
 import { useStore } from './store'
 
 const store = useStore()
+
+const parent = useParentElement()
+onClickOutside(parent, store.toggleToolbars)
 </script>
 
 <template>
-  <TriggerOverlay @click="store.toggleToolbars" />
   <AppHeader :active="store.isToolbarsShow" />
   <AppFooter :active="store.isToolbarsShow" />
   <DarkModeOverlay :active="store.isDarkMode" />
