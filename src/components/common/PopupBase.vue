9f6ae582863921d3ce354dc84e95ea1d271bf22c
DonkeyBear <35475647+DonkeyBear@users.noreply.github.com>
2025-10-22 17:13:11 +0800

    feat: 新增 PopupBase 以便複用

---
+++ src/components/common/PopupBase.vue
@@ -0,0 +1,25 @@
+<script setup>
+const modelValue = defineModel()
+</script>
+
+<template>
+  <Transition name="slide-and-fade">
+    <div v-show="modelValue" class="plus91-popup">
+      <slot />
+    </div>
+  </Transition>
+</template>
+
+<style scoped lang="scss">
+@import '@/styles/mixins.scss';
+
+@include transition-slide-and-fade;
+
+.plus91-popup {
+  @include popup;
+
+  $padding-x: 1rem;
+  padding-left: $padding-x;
+  padding-right: $padding-x;
+}
+</style>
