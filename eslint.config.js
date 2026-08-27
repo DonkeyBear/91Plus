16fab24df84424d82aa0c069526eea56fc7a0c5a
DonkeyBear <35475647+DonkeyBear@users.noreply.github.com>
2026-08-27 10:33:54 +0800

    chore: 更新工作區配置

--- eslint.config.js
+++ eslint.config.js
@@ -3,10 +3,11 @@ import antfu from '@antfu/eslint-config'
 export default antfu({
   formatters: true,
   vue: true,
+  rules: {
+    'jsdoc/require-returns-description': 'off',
+  },
   languageOptions: {
-    globals: {
-      $: 'readonly', // 使用 window 內的 jQuery
-    },
+    globals: {},
   },
 }, {
   files: ['**/*.vue'],
